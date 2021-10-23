5a43d0e229abac2ff09bffc123d9196958600c41
Vanshagra <vanshdeep.rai@gmail.com>
2021-10-23 23:42:53 +0530

    Changes done

--- Space Invaders Game/main.py
+++ Space Invaders Game/main.py
@@ -1,26 +1,41 @@
+import random
+
 import pygame
+
 # Initializing the pygame
 pygame.init()
 
-#Creating the screen ( Width , Height)
+# Creating the screen ( Width , Height)
 screen = pygame.display.set_mode((800, 600))
-
 
 # Setting the Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("space-ship.png")
 pygame.display.set_icon(icon)
 
-
 # Setting Up the Player
 playerImg = pygame.image.load("spaceship.png")
 playerx = 370
 playery = 500
+playerx_update = 0
+
+
+# Setting up the enemy
+enemyImg = pygame.image.load("alien.png")
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 450)
+enemyX_update = 1
+# enemyY_update = 0
+
 
 # Player function
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
-def player():
-    screen.blit(playerImg, (playerx, playery))
+
+# Enemy function
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # Game Loop
@@ -32,6 +47,43 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerx_update = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerx_update = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                playerx_update = 0
+            if event.key == pygame.K_RIGHT:
+                playerx_update = 0
 
-    player()
+    # Preventing Player from going outside
+    if playerx < 0:
+        playerx = 0
+    if playerx > 735:
+        playerx = 735
+
+    # Moving the Enemy
+    enemyX += enemyX_update
+    if enemyX > 735:
+        enemyX = 735
+        enemyX_update = -1
+        enemyY += 10
+    if enemyX < 0:
+        enemyX = 0
+        enemyX_update = 1
+        enemyY += 10
+
+    # Crashing of both the parties
+    if playerx >= enemyX and playery <= enemyY:
+        enemyX = 360
+        enemyX_update = 0
+        enemyY = 50
+
+    playerx += playerx_update
+    player(playerx, playery)
+
+
+    enemy(enemyX, enemyY)
     pygame.display.update()
